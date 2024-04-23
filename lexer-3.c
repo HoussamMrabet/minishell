b1f8b98a51615827091e6922e9405c6e1d207da7
HoussamMrabet <dodslim100@gmail.com>
2024-04-23 05:45:20 +0100

    fix norm

--- lexer-3.c
+++ lexer-3.c
@@ -6,17 +6,56 @@
 /*   By: hmrabet <hmrabet@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 10:11:54 by hmrabet           #+#    #+#             */
-/*   Updated: 2024/04/23 05:24:49 by hmrabet          ###   ########.fr       */
+/*   Updated: 2024/04/23 05:43:51 by hmrabet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	between_parenth(char *input, int *i);
+
+static inline int	check_signs(char *input, int i)
+{
+	return ((input[i] == '<' && input[i + 1] == '<')
+		|| (input[i] == '>' && input[i + 1] == '>')
+		|| (input[i] == '|' && input[i + 1] == '|')
+		|| (input[i] == '&' && input[i + 1] == '&'));
+}
+
+static int	between_parenth2(char *input, int *i)
+{
+	if (check_signs(input, *i))
+	{
+		*i += 2;
+		while (input[*i] == ' ' || input[*i] == '\t')
+			(*i)++;
+		if (input[*i] == ')')
+			return (1);
+	}
+	else if (input[*i] == '<' || input[*i] == '>' || input[*i] == '|')
+	{
+		(*i)++;
+		while (input[*i] == ' ' || input[*i] == '\t')
+			(*i)++;
+		if (input[*i] == ')')
+			return (1);
+	}
+	else if (input[*i] == '(')
+	{
+		(*i)++;
+		if (between_parenth(input, i))
+			return (1);
+	}
+	else
+		(*i)++;
+	return (0);
+}
+
 static int	between_parenth(char *input, int *i)
 {
 	while (input[*i] == ' ' || input[*i] == '\t')
 		(*i)++;
-	if (input[*i] && ((input[*i] == '|' && input[(*i) + 1] == '|') || (input[*i] == '&' && input[(*i) + 1] == '&') || input[*i] == '|'))
+	if (input[*i] && (input[*i] == '|' || input[*i] == '&'))
 		return (1);
 	while (input[*i] && input[*i] != ')')
 	{
@@ -34,34 +73,39 @@ static int	between_parenth(char *input, int *i)
 				(*i)++;
 			(*i)++;
 		}
-		else if ((input[*i] == '<' && input[(*i) + 1] == '<') || (input[*i] == '>' && input[(*i) + 1] == '>') || (input[*i] == '|' && input[(*i) + 1] == '|') || (input[*i] == '&' && input[(*i) + 1] == '&'))
-		{
-			*i += 2;
-			while (input[*i] == ' ' || input[*i] == '\t')
-				(*i)++;
-			if (input[*i] == ')')
-				return (1);
-		}
-		else if (input[*i] == '<' || input[*i] == '>' || input[*i] == '|')
-		{
-			(*i)++;
-			while (input[*i] == ' ' || input[*i] == '\t')
-				(*i)++;
-			if (input[*i] == ')')
-				return (1);
-		}
-		else if (input[*i] == '(')
-		{
-			(*i)++;
-			if (between_parenth(input, i))
-				return (1);
-		}
-		else
-		{
-			(*i)++;
-		}
+		else if (between_parenth2(input, i))
+			return (1);
 	}
 	(*i)++;
+	return (0);
+}
+
+static int	syntax_invalid2(char *input, int *i)
+{
+	if (check_signs(input, *i))
+	{
+		*i += 2;
+		while (input[*i] == ' ' || input[*i] == '\t')
+			(*i)++;
+		if (!input[*i])
+			return (1);
+	}
+	else if (input[*i] == '<' || input[*i] == '>' || input[*i] == '|')
+	{
+		(*i)++;
+		while (input[*i] == ' ' || input[*i] == '\t')
+			(*i)++;
+		if (!input[*i])
+			return (1);
+	}
+	else if (input[*i] == '(')
+	{
+		(*i)++;
+		if (between_parenth(input, i))
+			return (1);
+	}
+	else
+		(*i)++;
 	return (0);
 }
 
@@ -72,50 +116,24 @@ int	syntax_invalid(char *input)
 	i = 0;
 	while (input[i] == ' ' || input[i] == '\t')
 		i++;
-	if (input[i] && ((input[i] == '|' && input[i + 1] == '|') || (input[i] == '&' && input[i + 1] == '&') || input[i] == '|'))
+	if (input[i] && (input[i] == '|' || input[i] == '&'))
 		return (1);
 	while (input[i])
 	{
 		if (input[i] == '\'')
 		{
 			i++;
-			while (input[i] && input[i] != '\'')
-				i++;
-			i++;
+			while (input[i] && input[i++] != '\'')
+				;
 		}
 		else if (input[i] == '"')
 		{
 			i++;
-			while (input[i] && input[i] != '"')
-				i++;
-			i++;
+			while (input[i] && input[i++] != '"')
+				;
 		}
-		else if ((input[i] == '<' && input[i + 1] == '<') || (input[i] == '>' && input[i + 1] == '>') || (input[i] == '|' && input[i + 1] == '|') || (input[i] == '&' && input[i + 1] == '&'))
-		{
-			i += 2;
-			while (input[i] == ' ' || input[i] == '\t')
-				i++;
-			if (!input[i])
-				return (1);
-		}
-		else if (input[i] == '<' || input[i] == '>' || input[i] == '|')
-		{
-			i++;
-			while (input[i] == ' ' || input[i] == '\t')
-				i++;
-			if (!input[i])
-				return (1);
-		}
-		else if (input[i] == '(')
-		{
-			i++;
-			if (between_parenth(input, &i))
-				return (1);
-		}
-		else
-		{
-			i++;
-		}
+		else if (syntax_invalid2(input, &i))
+			return (1);
 	}
 	return (0);
 }
