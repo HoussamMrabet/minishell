007c5f1e861384a97559b7a411ae1192decf1b44
HoussamMrabet <dodslim100@gmail.com>
2024-04-23 19:18:32 +0100

    add exit status expand '0'

--- parser/parser-0.c
+++ parser/parser-0.c
@@ -6,7 +6,7 @@
 /*   By: hmrabet <hmrabet@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 06:41:00 by hmrabet           #+#    #+#             */
-/*   Updated: 2024/04/23 19:09:53 by hmrabet          ###   ########.fr       */
+/*   Updated: 2024/04/23 19:17:32 by hmrabet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,11 @@ void	handle_text_expand(t_minishell *minishell, char **tokens)
 				value = ft_strjoin(value, ft_itoa(&minishell->tmp, 32467), &minishell->tmp);
 				i++;
 			}
+			else if ((*tokens)[i + 1] == '?')
+			{
+				value = ft_strjoin(value, ft_itoa(&minishell->tmp, minishell->exit_status), &minishell->tmp);
+				i++;
+			}
 			else
 			{
 				j = 1;
@@ -67,6 +72,66 @@ void	handle_text_expand(t_minishell *minishell, char **tokens)
 	*tokens = value;
 }
 
+void	handle_quote_expand(t_minishell *minishell, char **tokens)
+{
+	int			i;
+	int			j;
+	char		*tmp;
+	char		*value;
+	t_bool		first;
+
+	first = TRUE;
+	i = 1;
+	j = 0;
+	value = ft_strdup("", &minishell->tmp);
+	while ((*tokens)[i] != '"')
+	{
+		if ((*tokens)[i] == '$')
+		{
+			// if (first && i != 0)
+			// {
+			// 	value = ft_substr(&minishell->tmp, (*tokens), 0, i - 1);
+			// 	first = FALSE;
+			// }
+			if ((*tokens)[i + 1] == '"')
+				value = ft_strjoin(value, "$", &minishell->tmp);
+			else if ((*tokens)[i + 1] == '$')
+			{
+				value = ft_strjoin(value, ft_itoa(&minishell->tmp, 32467), &minishell->tmp);
+				i++;
+			}
+			else if ((*tokens)[i + 1] == '?')
+			{
+				value = ft_strjoin(value, ft_itoa(&minishell->tmp, minishell->exit_status), &minishell->tmp);
+				i++;
+			}
+			else
+			{
+				j = 1;
+				while ((*tokens)[i + j] != '"' && (*tokens)[i + j] != '$')
+					j++;
+				j--;
+				tmp = get_env_value(minishell, ft_substr(&minishell->tmp, (*tokens) + (i + 1), 0, j));
+				if (!tmp)
+					tmp = ft_strdup("", &minishell->tmp);
+				value = ft_strjoin(value, tmp, &minishell->tmp);
+				i += j;
+			}
+		}
+		else
+		{
+			j = 0;
+			while ((*tokens)[i + j] != '"' && (*tokens)[i + j] != '$')
+				j++;
+			tmp = ft_substr(&minishell->tmp, (*tokens) + i, 0, j);
+			value = ft_strjoin(value, tmp, &minishell->tmp);
+			i += j - 1;
+		}
+		i++;
+	}
+	*tokens = value;
+}
+
 void	replace_expand_values(t_minishell *minishell, t_tokenizer **tokens)
 {
 	t_tokenizer	*token;
@@ -76,8 +141,8 @@ void	replace_expand_values(t_minishell *minishell, t_tokenizer **tokens)
 	{
 		if (token->type == TEXT)
 			handle_text_expand(minishell, &token->token);
-		// else if (token->type == D_QUOTE)
-		// 	handle_quote_expand();
+		else if (token->type == D_QUOTE)
+			handle_quote_expand(minishell, &token->token);
 		token = token->next;
 	}
 }
