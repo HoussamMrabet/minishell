50a7bc758dfe65c3c8e9a805a58610c59c349321
HoussamMrabet <dodslim100@gmail.com>
2024-06-10 23:04:23 +0100

    here doc

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hmrabet <hmrabet@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 22:51:44 by hmrabet           #+#    #+#             */
-/*   Updated: 2024/05/11 19:26:02 by hmrabet          ###   ########.fr       */
+/*   Updated: 2024/06/10 22:39:18 by hmrabet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,71 @@
 void	leaks(void)
 {
 	system("leaks minishell");
+}
+
+char	*get_del(char *input)
+{
+	char	*del;
+	int		j;
+	int		i;
+
+	j = 0;
+	i = 0;
+	while (input[i] && input[i] != ' ' && input[i] != '<' && input[i] != '>' && input[i] != '|' && input[i] != '&' && input[i] != '(' && input[i] != ')')
+	{
+		if (input[i] != '\'' && input[i] != '"')
+			j++;
+		i++;
+	}
+	i = 0;
+	del = malloc(j + 1);
+	if (!del || j == 0)
+		return (NULL);
+	j = 0;
+	while (input[i] && input[i] != ' ' && input[i] != '<' && input[i] != '>' && input[i] != '|' && input[i] != '&' && input[i] != '(' && input[i] != ')')
+	{
+		if (input[i] != '\'' && input[i] != '"')
+		{
+			del[j] = input[i];
+			j++;
+		}
+		i++;
+	}
+	del[j] = '\0';
+	return (del);
+}
+
+void	here_doc_err(char *input)
+{
+	char	*delim;
+	char	*del;
+	int		i;
+
+	i = 0;
+	del = NULL;
+	while (input[i])
+	{
+		if (input[i] == '<' && input[i + 1] == '<')
+		{
+			i += 2;
+			while (input[i] == ' ')
+				i++;
+			delim = get_del(input + i);
+			if (delim)
+			{
+				while (1)
+				{
+					free(del);
+					del = readline("> ");
+					if (!del || !ft_strcmp(delim, del))
+						break ;
+				}
+				free(del);
+			}
+			free(delim);
+		}
+		i++;
+	}
 }
 
 int	main(int c, char **v, char **env)
@@ -45,6 +110,7 @@ int	main(int c, char **v, char **env)
 			{
 				exit_status(258, TRUE);
 				ft_putstr_fd("Syntax error !\n", 2);
+				here_doc_err(input);
 				ft_free(&minishell.local);
 				continue ;
 			}
