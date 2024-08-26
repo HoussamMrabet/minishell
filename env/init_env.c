435925b6ca066a78ac8296f90fc3a5ee059b121d
HoussamMrabet <dodslim100@gmail.com>
2024-08-26 05:54:25 +0100

    protect ft_getcwd

--- env/init_env.c
+++ env/init_env.c
@@ -6,7 +6,7 @@
 /*   By: hmrabet <hmrabet@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 09:53:45 by hmrabet           #+#    #+#             */
-/*   Updated: 2024/08/23 08:45:33 by hmrabet          ###   ########.fr       */
+/*   Updated: 2024/08/26 05:53:16 by hmrabet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	init_default_env(t_minishell *m, char **env)
 	m->env[i] = NULL;
 	m->fake_env[i] = NULL;
 	m->_ = ft_strdup("minishell", m, &m->global);
-	m->pwd = ft_strdup(ft_getcwd(m), m, &m->global);
+	m->pwd = ft_strdup(get_env_value(m, "PWD"), m, &m->global);
 	m->home = ft_strdup("/Users/hmrabet", m, &m->global);
 }
 
@@ -42,6 +42,8 @@ void	init_custom_env(t_minishell *m)
 	m->env = ft_malloc(m, &m->global, sizeof(char *) * 4);
 	m->fake_env = ft_malloc(m, &m->global, sizeof(char *) * 4);
 	(1) && (cwd = NULL, cwd = ft_getcwd(m));
+	if (!cwd)
+		ft_exit("minishell: getcwd failed", 1, m);
 	m->env[0] = ft_strjoin("OLDPWD", "", m, &m->global);
 	m->env[1] = ft_strjoin("PWD=", cwd, m, &m->global);
 	m->env[2] = ft_strjoin("SHLVL=", "1", m, &m->global);
