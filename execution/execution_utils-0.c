077a64e8bec414ea54a14376ff7487a86b90fa02
M-elhamdaoui <melhamdaoui811@gmail.com>
2024-08-27 09:36:06 +0100

    fix redirections in builtins

--- execution/execution_utils-0.c
+++ execution/execution_utils-0.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execution_utils-0.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmrabet <hmrabet@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 18:02:14 by hmrabet           #+#    #+#             */
-/*   Updated: 2024/08/26 18:02:43 by hmrabet          ###   ########.fr       */
+/*   Updated: 2024/08/27 09:29:41 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ int	close_fds_child_btn(int fdin, int fdout, t_exec *tree)
 {
 	if (fdin == tree->fdin && fdout == tree->fdout)
 		return (0);
-	if (fdin != tree->fdin && fdin != 0)
-		return (catch_error_builtin(close(fdin), "close"), 1);
-	else if (fdout != tree->fdout && fdout != 1)
-		return (catch_error_builtin(close(fdout), "close"), 1);
+	if (fdin != tree->fdin && fdin != 0 && close(fdin) == -1)
+		return (catch_error_builtin(errno, "close"), 1);
+	else if (fdout != tree->fdout && fdout != 1 && close(fdout) == -1)
+		return (catch_error_builtin(errno, "close"), 1);
 	return (0);
 }
 
