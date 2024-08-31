8b33fb267abf9d322c2dba484c6e9a509f9adb55
HoussamMrabet <dodslim100@gmail.com>
2024-08-31 12:07:30 +0100

    handle some issues

--- env/init_env.c
+++ env/init_env.c
@@ -6,7 +6,7 @@
 /*   By: hmrabet <hmrabet@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 09:53:45 by hmrabet           #+#    #+#             */
-/*   Updated: 2024/08/27 07:18:24 by hmrabet          ###   ########.fr       */
+/*   Updated: 2024/08/31 12:06:28 by hmrabet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,27 @@ void	init_default_env(t_minishell *m, char **env)
 	m->fake_env[i] = NULL;
 	m->_ = ft_strdup("minishell", m, &m->global);
 	m->pwd = ft_strdup(get_env_value(m, "PWD"), m, &m->global);
-	m->home = ft_strdup("/Users/hmrabet", m, &m->global);
+	m->home = ft_strdup(getenv("HOME"), m, &m->global);
+}
+
+static char	*ft_gethome(t_minishell *m, char *pwd)
+{
+	char	*home;
+	int		count;
+	int		i;
+
+	i = 0;
+	count = 0;
+	if (!pwd)
+		return (NULL);
+	while (pwd[i] && count != 3)
+	{
+		if (pwd[i] == '/')
+			count++;
+		i++;
+	}
+	home = ft_substr(m, pwd, 0, i);
+	return (home);
 }
 
 void	init_custom_env(t_minishell *m)
@@ -59,5 +79,5 @@ void	init_custom_env(t_minishell *m)
 	m->fake_env[2] = ft_strjoin("_=", "/usr/bin/env", m, &m->global);
 	m->fake_env[3] = NULL;
 	m->pwd = ft_strdup(m->env[1], m, &m->global);
-	m->home = ft_strdup("/Users/hmrabet", m, &m->global);
+	m->home = ft_strdup(ft_gethome(m, cwd), m, &m->global);
 }
